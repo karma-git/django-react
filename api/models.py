a6b1da5870ecd614e684ea00cadc00e97f446046
Andrei Horbach <andrewhorbach@gmail.com>
2021-10-17 20:06:58 +0300

    chore: customize qs

--- api/models.py
+++ api/models.py
@@ -9,6 +9,24 @@ class BlogCategory(models.Model):
     def __str__(self):
         return f'categore=<{self.name}>'
 
+
+class BlogPostManager(models.Manager):
+
+    def get_queryset(self):
+        return BlogPostQuerySet(self.model, using=self._db)
+
+    def all(self):
+        return self.get_queryset().filter(in_archive=False)
+    
+    def find_by_title_in_qs(self, post_title):
+        return self.get_queryset().find_by_title_in_qs(post_title)
+
+class BlogPostQuerySet(models.QuerySet):
+
+    def find_by_title_in_qs(self, post_title):
+        return self.filter(title__icontains=post_title)
+
+
 class BlogPost(models.Model):
 
     blog_category = models.ForeignKey(BlogCategory, verbose_name='Category name', on_delete=models.CASCADE)
@@ -18,15 +36,8 @@ class BlogPost(models.Model):
     image = models.ImageField(upload_to='blog_posts/', blank=True, null=True)
     pub_date = models.DateField(auto_now=True)
     in_archive = models.BooleanField(default=False)
+    objects = BlogPostManager()
 
     def __str__(self):
         return f'post=<{self.title}>,from=<{self.blog_category.id}>'
 
-
-class BlogPostManager(models.Manager):
-
-    def get_queryset(self):
-        return super().get_queryset()
-
-    def all(self):
-        return self.get_queryset().filter(in_archive=False)
