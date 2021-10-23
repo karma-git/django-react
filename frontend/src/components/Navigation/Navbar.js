c00f354965f848fe2dc7d1ec7bce2e6cc1f51546
Andrew Horbach <andrewhorbach@gmail.com>
2021-10-23 20:54:53 +0300

    chore: genereate categories in navbar

---
+++ frontend/src/components/Navigation/Navbar.js
@@ -0,0 +1,45 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
+function Navbar() {
+
+    // Объявляется пустая переменная
+    const [categories, setCategories] = useState([])
+
+    // Выполняется GET запрос на EP
+    useEffect( () => {
+    axios({
+        method: "GET",
+        url: "http://127.0.0.1:8000/api/category/"
+    }).then(responce => {
+        setCategories(responce.data)
+    })
+    }, [] )
+  
+  return ( 
+    <div className="App">
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+            
+            {categories.map(c=>(
+                <a class="nav-link" href="#" key={c.id}>{c.name}</a>
+            ))}
+            
+        </div>
+        </div>
+    </div>
+    </nav>
+      
+    </div>
+  );
+}
+
+export default Navbar;
